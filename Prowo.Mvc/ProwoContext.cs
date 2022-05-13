8b39f7191c22c675325ffd7fcb272b4680c7cddc
Johannes Egger <j.egger@posteo.at>
2022-05-13 05:12:47 +0200

    Try MVC project

---
+++ Prowo.Mvc/ProwoContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Prowo.Mvc.DB;
+
+namespace Prowo.Mvc
+{
+    public class ProwoContext : DbContext
+    {
+        public ProwoContext(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<Project> Projects { get; set; }
+    }
+}
