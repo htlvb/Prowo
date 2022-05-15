73938f534fec059fcf89c5882e573bf4fedccbcd
johannesegger <j.egger@posteo.at>
2022-05-15 21:52:39 +0200

    Use only primitive types for DbProject

--- Prowo.BlazorServer/Database/DbProject.cs
+++ Prowo.BlazorServer/Database/DbProject.cs
@@ -7,15 +7,15 @@ namespace Prowo.BlazorServer.Database
     [JsonObject(NamingStrategyType = typeof(CamelCaseNamingStrategy))]
     public class DbProject
     {
-        public Guid Id { get; set; }
+        public string Id { get; set; }
         public string Title { get; set; }
         public string Description { get; set; }
         public string Location { get; set; }
         public string OrganizerId { get; set; }
         public string[] CoOrganizerIds { get; set; }
-        public DateOnly Date { get; set; }
-        public TimeOnly StartTime { get; set; }
-        public TimeOnly? EndTime { get; set; }
+        public string Date { get; set; }
+        public string StartTime { get; set; }
+        public string? EndTime { get; set; }
         public int MaxParticipants { get; set; }
     }
 }
