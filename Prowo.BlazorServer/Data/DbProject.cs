8a410dab55612bb3ab12798f53e751d455b84515
johannesegger <j.egger@posteo.at>
2022-05-18 13:55:07 +0200

    Add buttons for registering for and deregistering from an event; unify naming

--- Prowo.BlazorServer/Data/DbProject.cs
+++ Prowo.BlazorServer/Data/DbProject.cs
@@ -1,5 +1,8 @@
 ﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
 using Newtonsoft.Json.Serialization;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Prowo.BlazorServer.Data
 {
@@ -15,6 +18,38 @@ namespace Prowo.BlazorServer.Data
         public string Date { get; set; }
         public string StartTime { get; set; }
         public string? EndTime { get; set; }
-        public int MaxParticipants { get; set; }
+        public int MaxAttendees { get; set; }
+        public RegistrationEvent[] RegistrationEvents { get; set; }
+
+        public List<string> CalculateActualAttendees()
+        {
+            List<string> result = new();
+            foreach (var entry in RegistrationEvents)
+            {
+                if (entry.Action == RegistrationAction.Register)
+                {
+                    result.Add(entry.UserId);
+                }
+                else if (entry.Action == RegistrationAction.Deregister)
+                {
+                    result.Remove(entry.UserId);
+                }
+            }
+            return result;
+        }
+
+        [JsonObject(NamingStrategyType = typeof(CamelCaseNamingStrategy))]
+        public class RegistrationEvent
+        {
+            public string UserId { get; set; }
+            public RegistrationAction Action { get; set; }
+        }
+
+        [JsonConverter(typeof(StringEnumConverter), /* camelCaseText */ true)]
+        public enum RegistrationAction
+        {
+            Register,
+            Deregister
+        }
     }
 }
--- Prowo.BlazorServer/Data/ProjectStore.cs
+++ Prowo.BlazorServer/Data/ProjectStore.cs
@@ -9,9 +9,7 @@ namespace Prowo.BlazorServer.Data
     {
         private readonly CosmosClient cosmosClient;
 
-        private Container ProjectContainer => cosmosClient
-            .GetDatabase("ProjectsDB")
-            .GetContainer("Project");
+        private Container ProjectContainer => cosmosClient.GetContainer("ProjectsDB", "Project");
 
         public ProjectStore(CosmosClient cosmosClient)
         {
@@ -37,9 +35,65 @@ namespace Prowo.BlazorServer.Data
                 .ReadItemAsync<DbProject>(projectId, new PartitionKey(projectId));
         }
 
-        public async Task CreateOrUpdate(DbProject project)
+        public async Task CreateProject(DbProject project)
         {
-            await ProjectContainer.UpsertItemAsync(project);
+            await ProjectContainer.CreateItemAsync(project);
+        }
+
+        public async Task UpdateProject(DbProject project)
+        {
+            await ProjectContainer.PatchItemAsync<DbProject>(
+                project.Id,
+                new PartitionKey(project.Id),
+                new[]
+                {
+                    PatchOperation.Replace("/title", project.Title),
+                    PatchOperation.Replace("/description", project.Description),
+                    PatchOperation.Replace("/location", project.Location),
+                    PatchOperation.Replace("/organizerId", project.OrganizerId),
+                    PatchOperation.Replace("/coOrganizerIds", project.CoOrganizerIds),
+                    PatchOperation.Replace("/date", project.Date),
+                    PatchOperation.Replace("/startTime", project.StartTime),
+                    PatchOperation.Replace("/endTime", project.EndTime),
+                    PatchOperation.Replace("/maxAttendees", project.MaxAttendees)
+                }
+            );
+        }
+
+        public async Task AddAttendee(string projectId, string userId)
+        {
+            DbProject.RegistrationEvent registrationEvent = new()
+            {
+                UserId = userId,
+                Action = DbProject.RegistrationAction.Register
+            };
+            var response = await ProjectContainer.PatchItemAsync<DbProject>(
+                projectId,
+                new PartitionKey(projectId),
+                new[]
+                {
+                    PatchOperation.Add("/registrationEvents/-", registrationEvent)
+                }
+            );
+            // TODO update UI model because new attendees could have registered in the meantime
+        }
+
+        public async Task RemoveAttendee(string projectId, string userId)
+        {
+            DbProject.RegistrationEvent registrationEvent = new()
+            {
+                UserId = userId,
+                Action = DbProject.RegistrationAction.Deregister
+            };
+            var response = await ProjectContainer.PatchItemAsync<DbProject>(
+                projectId,
+                new PartitionKey(projectId),
+                new[]
+                {
+                    PatchOperation.Add("/registrationEvents/-", registrationEvent)
+                }
+            );
+            // TODO update UI model because new attendees could have registered in the meantime
         }
 
         public void Dispose()
