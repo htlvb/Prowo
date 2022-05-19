e8b41ef9b0cadc84b4c1b192a3a30fce09bb6d11
johannesegger <j.egger@posteo.at>
2022-05-19 22:21:32 +0200

    Remove old web projects

--- Prowo.Mvc/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Prowo.Mvc.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
