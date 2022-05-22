6bd4d4b0882fb21804b80849778fb3c026bf082d
Johannes Egger <j.egger@posteo.at>
2022-05-22 19:45:01 +0200

    Add script to create Azure resources

---
+++ create-azure-resources.ps1
@@ -0,0 +1,15 @@
+$subscriptionName = "Pay-As-You-Go"
+$resourceGroupName = "prowo-rg"
+$accountName = "prowo-acdb2"
+$regionName = "eastus" # westeurope
+$dbName = "ProjectsDB"
+$containerName = "Project"
+
+az login
+az cosmosdb create --name $accountName --resource-group $resourceGroupName --subscription $subscriptionName --locations regionName=$regionName
+az cosmosdb sql database create --name $dbName --account-name $accountName --resource-group $resourceGroupName
+az cosmosdb sql container create --name $containerName --partition-key-path /id --max-throughput 1000 --account-name $accountname --resource-group $resourceGroupName --database-name $dbName
+
+az cosmosdb sql container delete --name $containerName --account-name $accountname --resource-group $resourceGroupName --database-name $dbName --yes
+az cosmosdb sql database delete --name $dbName --account-name $accountname --resource-group $resourceGroupName --yes
+az cosmosdb delete --name $accountName --resource-group $resourceGroupName --yes
