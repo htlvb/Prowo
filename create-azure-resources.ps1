56ede97e1722bf3e30482b100024b50e8bc6d941
Johannes Egger <j.egger@posteo.at>
2022-05-23 13:17:55 +0200

    Use free tier CosmosDB account

--- create-azure-resources.ps1
+++ create-azure-resources.ps1
@@ -6,7 +6,7 @@ $dbName = "ProjectsDB"
 $containerName = "Project"
 
 az login
-az cosmosdb create --name $accountName --resource-group $resourceGroupName --subscription $subscriptionName --locations regionName=$regionName
+az cosmosdb create --name $accountName --enable-free-tier true --resource-group $resourceGroupName --subscription $subscriptionName --locations regionName=$regionName
 az cosmosdb sql database create --name $dbName --account-name $accountName --resource-group $resourceGroupName
 az cosmosdb sql container create --name $containerName --partition-key-path /id --max-throughput 1000 --account-name $accountname --resource-group $resourceGroupName --database-name $dbName
 
