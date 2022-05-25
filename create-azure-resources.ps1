ebb4743ecc23ac15bb43305710098f164ab47e10
johannesegger <j.egger@posteo.at>
2022-05-25 14:20:27 +0200

    Use $accountName when getting acdb connection string

--- create-azure-resources.ps1
+++ create-azure-resources.ps1
@@ -17,7 +17,7 @@ az cosmosdb create --name $accountName --enable-free-tier true --resource-group 
 az cosmosdb sql database create --name $dbName --account-name $accountName --resource-group $resourceGroupName
 az cosmosdb sql container create --name $containerName --partition-key-path /id --max-throughput 1000 --account-name $accountname --resource-group $resourceGroupName --database-name $dbName
 az appservice plan create --name $appServicePlanName --location $regionName --is-linux --sku FREE --resource-group $resourceGroupName
-$cosmosDbConnectionString = az cosmosdb keys list --name prowo-acdb --resource-group $resourceGroupName --type connection-strings --query connectionStrings[0].connectionString --output tsv
+$cosmosDbConnectionString = az cosmosdb keys list --name $accountName --resource-group $resourceGroupName --type connection-strings --query connectionStrings[0].connectionString --output tsv
 az webapp create --name $webAppName --runtime DOTNETCORE:6.0 --plan $appServicePlanName --resource-group $resourceGroupName
 az webapp config connection-string set --settings "CosmosDb=$cosmosDbConnectionString" --connection-string-type Custom --name $webAppName --resource-group $resourceGroupName
 az webapp config appsettings set --settings "AzureAD__ClientSecret=$clientSecret" --name $webAppName --resource-group $resourceGroupName
