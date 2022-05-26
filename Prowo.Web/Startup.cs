402e4537ea9ca9b22f16383a9de62857df1998f3
johannesegger <j.egger@posteo.at>
2022-05-26 22:17:06 +0200

    Forward all headers

--- Prowo.Web/Startup.cs
+++ Prowo.Web/Startup.cs
@@ -29,8 +29,7 @@ namespace Prowo.Web
         {
             services.Configure<ForwardedHeadersOptions>(options =>
             {
-                options.ForwardedHeaders =
-                    ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto;
+                options.ForwardedHeaders = ForwardedHeaders.All;
             });
 
             services.AddLocalization();
