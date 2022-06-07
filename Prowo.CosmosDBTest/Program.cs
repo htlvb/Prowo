d5f79d1eae368a91d98f83aaf93e9603a1f77fc5
Johannes Egger <j.egger@posteo.at>
2022-06-07 14:39:21 +0200

    Add script to fix existing closing dates

--- Prowo.CosmosDBTest/Program.cs
+++ Prowo.CosmosDBTest/Program.cs
@@ -8,70 +8,105 @@ var configuration = new ConfigurationBuilder()
 string connectionString = configuration.GetConnectionString("CosmosDb");
 using CosmosClient cosmosClient = new(connectionString);
 
-var containerResponse = await cosmosClient
-    .GetDatabase("ProjectsDB")
-    .DefineContainer("ConsoleTest", "/id")
-    .CreateAsync();
-var container = containerResponse.Container;
-var itemResponse = await container.CreateItemAsync(
-    new Item("1111", Array.Empty<string>())
-);
-var patch1ItemResponse = await container.PatchItemAsync<Item>(
-    itemResponse.Resource.id,
-    new PartitionKey(itemResponse.Resource.id),
-    new[]
-    {
-        PatchOperation.Add("/items/-", "Johannes"),
-        PatchOperation.Add("/items/-", "Sylvia")
-    }
-);
-var patch2ItemResponse = await container.PatchItemAsync<Item>(
-    itemResponse.Resource.id,
-    new PartitionKey(itemResponse.Resource.id),
-    new[]
-    {
-        PatchOperation.Add("/items/-", "Marie"),
-        PatchOperation.Add("/items/-", "Theo"),
-        PatchOperation.Add("/items/-", "Sarah")
-    }
-);
-var patch3ItemResponse = await container.PatchItemAsync<Item>(
-    itemResponse.Resource.id,
-    new PartitionKey(itemResponse.Resource.id),
-    new[]
-    {
-        PatchOperation.Remove("/items/1")
-    },
-    new PatchItemRequestOptions { FilterPredicate = $"FROM p WHERE p.items[1] = '{"Sylvia"}'" }
-);
+//await TestCollectionOperations();
+//await UpdateClosingDate();
 
-var patch4ItemResponse = await container.PatchItemAsync<Item>(
-    itemResponse.Resource.id,
-    new PartitionKey(itemResponse.Resource.id),
-    new[]
-    {
-        PatchOperation.Remove("/items/1")
-    },
-    new PatchItemRequestOptions { FilterPredicate = $"FROM p WHERE p.items[1] = '{"Marie"}'" }
-);
-
-try
+async Task TestCollectionOperations()
 {
-    var patch5ItemResponse = await container.PatchItemAsync<Item>(
+    var containerResponse = await cosmosClient
+        .GetDatabase("ProjectsDB")
+        .DefineContainer("ConsoleTest", "/id")
+        .CreateAsync();
+    var container = containerResponse.Container;
+    var itemResponse = await container.CreateItemAsync(
+        new Item("1111", Array.Empty<string>())
+    );
+    var patch1ItemResponse = await container.PatchItemAsync<Item>(
+        itemResponse.Resource.id,
+        new PartitionKey(itemResponse.Resource.id),
+        new[]
+        {
+            PatchOperation.Add("/items/-", "Johannes"),
+            PatchOperation.Add("/items/-", "Sylvia")
+        }
+    );
+    var patch2ItemResponse = await container.PatchItemAsync<Item>(
+        itemResponse.Resource.id,
+        new PartitionKey(itemResponse.Resource.id),
+        new[]
+        {
+            PatchOperation.Add("/items/-", "Marie"),
+            PatchOperation.Add("/items/-", "Theo"),
+            PatchOperation.Add("/items/-", "Sarah")
+        }
+    );
+    var patch3ItemResponse = await container.PatchItemAsync<Item>(
         itemResponse.Resource.id,
         new PartitionKey(itemResponse.Resource.id),
         new[]
         {
             PatchOperation.Remove("/items/1")
         },
-        new PatchItemRequestOptions { FilterPredicate = $"FROM p WHERE p.items[1] = '{"theo"}'" }
+        new PatchItemRequestOptions { FilterPredicate = $"FROM p WHERE p.items[1] = '{"Sylvia"}'" }
     );
-}
-catch (CosmosException e) when (e.StatusCode == System.Net.HttpStatusCode.PreconditionFailed)
-{
-    Console.WriteLine("Can't find theo");
+
+    var patch4ItemResponse = await container.PatchItemAsync<Item>(
+        itemResponse.Resource.id,
+        new PartitionKey(itemResponse.Resource.id),
+        new[]
+        {
+            PatchOperation.Remove("/items/1")
+        },
+        new PatchItemRequestOptions { FilterPredicate = $"FROM p WHERE p.items[1] = '{"Marie"}'" }
+    );
+
+    try
+    {
+        var patch5ItemResponse = await container.PatchItemAsync<Item>(
+            itemResponse.Resource.id,
+            new PartitionKey(itemResponse.Resource.id),
+            new[]
+            {
+                PatchOperation.Remove("/items/1")
+            },
+            new PatchItemRequestOptions { FilterPredicate = $"FROM p WHERE p.items[1] = '{"theo"}'" }
+        );
+    }
+    catch (CosmosException e) when (e.StatusCode == System.Net.HttpStatusCode.PreconditionFailed)
+    {
+        Console.WriteLine("Can't find theo");
+    }
+
+    var deleteContainerResponse = await container.DeleteContainerAsync();
+
 }
 
-var deleteContainerResponse = await container.DeleteContainerAsync();
+async Task UpdateClosingDate()
+{
+    var container = cosmosClient.GetContainer("ProjectsDB", "Project");
+
+    var query = container.GetItemQueryIterator<DbProject>();
+    while (query.HasMoreResults)
+    {
+        var response = await query.ReadNextAsync();
+        foreach (var dbProject in response)
+        {
+            if (dbProject.closingDate.TimeOfDay == TimeSpan.Zero)
+            {
+                Console.WriteLine($"{dbProject.id} - {dbProject.closingDate} ({dbProject.closingDate.Kind})");
+                await container.PatchItemAsync<DbProject>(
+                    dbProject.id,
+                    new PartitionKey(dbProject.id),
+                    new[]
+                    {
+                        PatchOperation.Replace("/closingDate", dbProject.closingDate.AddHours(-2)),
+                    }
+                );
+            }
+        }
+    }
+}
 
 record Item(string id, string[] items);
+
+record DbProject(string id, DateTime closingDate);
