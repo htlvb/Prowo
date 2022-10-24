a9c9a79cd286a0c22b77a881443931535b5e0b26
johannesegger <j.egger@posteo.at>
2022-10-24 20:24:34 +0200

    Update tailwindcss

--- init.sh
+++ init.sh
@@ -1,3 +1,3 @@
 mkdir -p ./.tools
-wget -O ./.tools/tailwindcss https://github.com/tailwindlabs/tailwindcss/releases/download/v3.0.24/tailwindcss-linux-x64
+wget -O ./.tools/tailwindcss https://github.com/tailwindlabs/tailwindcss/releases/download/v3.2.1/tailwindcss-linux-x64
 chmod +x ./.tools/tailwindcss
