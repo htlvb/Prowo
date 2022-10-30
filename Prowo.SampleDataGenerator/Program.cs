8450e99d2b0a20628883b5c5fe83d0ccdff3fd51
johannesegger <j.egger@posteo.at>
2022-10-30 07:42:26 +0100

    Replace sample data generator

--- Prowo.SampleDataGenerator/Program.cs
+++ Prowo.SampleDataGenerator/Program.cs
@@ -1,21 +1,20 @@
-﻿using Microsoft.Azure.Cosmos;
-using Microsoft.Extensions.Configuration;
+﻿using Microsoft.Extensions.Configuration;
+using Npgsql;
+using NpgsqlTypes;
 using System.Globalization;
 using System.Text.Json;
 using System.Text.RegularExpressions;
+
+NpgsqlConnection.GlobalTypeMapper.UseJsonNet();
+NpgsqlConnection.GlobalTypeMapper.MapEnum<RegistrationAction>("registration_action");
 
 var configuration = new ConfigurationBuilder()
     .AddUserSecrets<Program>()
     .Build();
 
-string connectionString = configuration.GetConnectionString("CosmosDb");
-using CosmosClient cosmosClient = new(connectionString);
-
-var container = cosmosClient.GetContainer("ProjectsDB", "Project");
-//await container.DeleteContainerAsync();
-//container = await cosmosClient
-//    .GetDatabase("ProjectsDB")
-//    .CreateContainerAsync("Project", "/id");
+string connectionString = configuration.GetConnectionString("PostgresqlDb");
+await using var dbConnection = new NpgsqlConnection(connectionString);
+await dbConnection.OpenAsync();
 
 var attendees = JsonDocument.Parse(File.ReadAllText("AttendeeCandidates.json"))
     .RootElement
@@ -57,43 +56,65 @@ var sampleProjects = JsonDocument.Parse(File.ReadAllText("SampleProjects.json"))
             Title = v.GetProperty("title").GetString()!,
             Description = v.GetProperty("description").GetString()!,
             Location = v.GetProperty("location").GetString()!,
+            Organizer = organizers[Random.Shared.Next(0, organizers.Count)],
+            CoOrganizers = organizers
+                .OrderBy(_ => Random.Shared.NextDouble())
+                .Take(Random.Shared.Next(0, 10))
+                .ToArray(),
             Date = date,
             StartTime = TimeSpan.ParseExact(startTimeString, "h\\:mm", CultureInfo.InvariantCulture),
             EndTime = endTimeString != null ? TimeSpan.ParseExact(endTimeString, "h\\:mm", CultureInfo.InvariantCulture) : default(TimeSpan?),
             ClosingDate = date.AddDays(Random.Shared.Next(-30, 1)).ToUniversalTime(),
-            MaxAttendees = maxAttendees
+            MaxAttendees = maxAttendees,
+            RegistrationEvents = attendees
+                .OrderBy(_ => Random.Shared.NextDouble())
+                .Take(Random.Shared.Next(0, maxAttendees * 2))
+                .Select(v => new
+                {
+                    userId = v.UserId,
+                    firstName = v.FirstName,
+                    lastName = v.LastName,
+                    @class = v.Class,
+                    timestamp = DateTime.UtcNow,
+                    action = RegistrationAction.Register
+                })
+                .ToArray()
         };
     });
+
+await using (var cmd = new NpgsqlCommand("DELETE FROM project", dbConnection))
+{
+    await cmd.ExecuteNonQueryAsync();
+}
+
 foreach (var project in sampleProjects.Take(10))
 {
-    await container.CreateItemAsync(new
+    await using (var cmd = new NpgsqlCommand("INSERT INTO project (id, title, description, location, organizer, co_organizers, date, start_time, end_time, closing_date, maxAttendees) VALUES (@id, @title, @description, @location, @organizer, @co_organizers, @date, @start_time, @end_time, @closing_date, @maxAttendees)", dbConnection))
     {
-        id = project.Id,
-        title = project.Title,
-        description = project.Description,
-        location = project.Location,
-        organizer = organizers[Random.Shared.Next(0, organizers.Count)],
-        coOrganizers = organizers
-            .OrderBy(_ => Random.Shared.NextDouble())
-            .Take(Random.Shared.Next(0, 10))
-            .ToArray(),
-        date = project.Date,
-        startTime = project.StartTime,
-        endTime = project.EndTime,
-        closingDate = project.ClosingDate,
-        maxAttendees = project.MaxAttendees,
-        registrationEvents = attendees
-            .OrderBy(_ => Random.Shared.NextDouble())
-            .Take(Random.Shared.Next(0, project.MaxAttendees * 2))
-            .Select(v => new
-            {
-                userId = v.UserId,
-                firstName = v.FirstName,
-                lastName = v.LastName,
-                @class = v.Class,
-                timestamp = DateTime.UtcNow,
-                action = "register"
-            })
-            .ToArray()
-    });
+        cmd.Parameters.AddWithValue("id", Guid.Parse(project.Id));
+        cmd.Parameters.AddWithValue("title", project.Title);
+        cmd.Parameters.AddWithValue("description", project.Description);
+        cmd.Parameters.AddWithValue("location", project.Location);
+        cmd.Parameters.AddWithValue("organizer", NpgsqlDbType.Json, project.Organizer);
+        cmd.Parameters.AddWithValue("co_organizers", NpgsqlDbType.Json, project.CoOrganizers);
+        cmd.Parameters.AddWithValue("date", project.Date);
+        cmd.Parameters.AddWithValue("start_time", project.StartTime);
+        cmd.Parameters.AddWithValue("end_time", (object?)project.EndTime ?? (object)DBNull.Value);
+        cmd.Parameters.AddWithValue("closing_date", project.ClosingDate);
+        cmd.Parameters.AddWithValue("maxAttendees", project.MaxAttendees);
+
+        await cmd.ExecuteScalarAsync();
+    }
+
+    await using (var cmd = new NpgsqlCommand("INSERT INTO registration_event (project_id, \"user\", action, timestamp) SELECT UNNEST(@project_id), UNNEST(@user), UNNEST(@action), UNNEST(@timestamp)", dbConnection))
+    {
+        cmd.Parameters.AddWithValue("project_id", Enumerable.Repeat(Guid.Parse(project.Id), project.RegistrationEvents.Length).ToArray());
+        cmd.Parameters.AddWithValue("user", NpgsqlDbType.Array | NpgsqlDbType.Json, project.RegistrationEvents.Select(v => new { user_id = v.userId, first_name = v.firstName, last_name = v.lastName }).ToArray());
+        cmd.Parameters.AddWithValue("action", project.RegistrationEvents.Select(v => v.action).ToArray());
+        cmd.Parameters.AddWithValue("timestamp", project.RegistrationEvents.Select(v => v.timestamp).ToArray());
+
+        await cmd.ExecuteNonQueryAsync();
+    }
 }
+
+enum RegistrationAction { Register, Deregister }
--- db-schema.sql
+++ db-schema.sql
@@ -2,7 +2,7 @@ DROP TABLE IF EXISTS registration;
 DROP TABLE IF EXISTS project;
 
 CREATE TABLE project(
-    id SERIAL PRIMARY KEY NOT NULL,
+    id UUID PRIMARY KEY NOT NULL,
     title VARCHAR NOT NULL,
     description VARCHAR NOT NULL,
     location VARCHAR(50) NOT NULL,
@@ -10,15 +10,16 @@ CREATE TABLE project(
     co_organizers JSON NOT NULL,
     date DATE NOT NULL,
     start_time TIME NOT NULL,
-    end_time TIME NOT NULL,
+    end_time TIME,
     closing_date TIMESTAMP NOT NULL,
     maxAttendees INT NOT NULL
 );
 
 CREATE TYPE registration_action AS ENUM ('register', 'deregister');
 CREATE TABLE registration_event(
-    project_id INT NOT NULL,
+    project_id UUID NOT NULL,
     "user" JSON NOT NULL,
+    timestamp TIMESTAMP NOT NULL,
     action registration_action NOT NULL,
     FOREIGN KEY(project_id) REFERENCES project(id) ON DELETE CASCADE
 );
