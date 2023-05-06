8f26fc855334e45d3eca08b6434113414ea64b1f
Johannes Egger <j.egger@posteo.at>
2023-05-06 13:06:14 +0200

    Fail init script if sub-command fails

--- init.sh
+++ init.sh
@@ -1,8 +1,8 @@
 mkdir -p ./.tools
-wget -O ./.tools/tailwindcss https://github.com/tailwindlabs/tailwindcss/releases/download/v3.2.1/tailwindcss-linux-x64
+wget -O ./.tools/tailwindcss https://github.com/tailwindlabs/tailwindcss/releases/download/v3.2.1/tailwindcss-linux-x64 || exit 1
 chmod +x ./.tools/tailwindcss
 
 pushd ./Prowo.WebAsm/Client
-dotnet tool restore
-dotnet libman restore
+dotnet tool restore || exit 1
+dotnet libman restore || exit 1
 popd
