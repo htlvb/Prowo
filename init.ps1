c915c1bf0b7a76fa8c63ff0f85e9c181fd2c802a
Johannes Egger <j.egger@posteo.at>
2023-05-06 12:52:32 +0200

    Add font awesome through libman

--- init.ps1
+++ init.ps1
@@ -1,2 +1,7 @@
 mkdir .\.tools -Force | Out-Null
 Invoke-WebRequest https://github.com/tailwindlabs/tailwindcss/releases/download/v3.2.1/tailwindcss-windows-x64.exe -OutFile .\.tools\tailwindcss.exe
+
+Push-Location .\Prowo.WebAsm\Client
+dotnet tool restore
+dotnet libman restore
+Pop-Location
--- init.sh
+++ init.sh
@@ -1,3 +1,8 @@
 mkdir -p ./.tools
 wget -O ./.tools/tailwindcss https://github.com/tailwindlabs/tailwindcss/releases/download/v3.2.1/tailwindcss-linux-x64
 chmod +x ./.tools/tailwindcss
+
+pushd ./Prowo.WebAsm/Client
+dotnet tool restore
+dotnet libman restore
+popd
