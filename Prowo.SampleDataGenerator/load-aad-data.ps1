836054c1b7fa41113e7d6c9cf2ee92cd6faf6088
Johannes Egger <j.egger@posteo.at>
2023-08-03 13:26:54 +0200

    Filter allowed users when generating sample data

--- Prowo.SampleDataGenerator/load-aad-data.ps1
+++ Prowo.SampleDataGenerator/load-aad-data.ps1
@@ -5,6 +5,7 @@ Connect-AzureAD
 $attendeeGroupId = (Get-AzureADGroup -Filter "displayName eq 'GrpSchueler'").ObjectId
 Get-AzureADGroupMember -ObjectId $attendeeGroupId -All $true `
     | Select-Object ObjectId,GivenName,Surname,Department,UserPrincipalName `
+    | Where-Object { $_.GivenName -and $_.Surname -and $_.Department -and $_.UserPrincipalName } `
     | ConvertTo-Json > .\AttendeeCandidates.json
 
 $organizerGroupId = (Get-AzureADGroup -Filter "displayName eq 'GrpLehrer'").ObjectId
