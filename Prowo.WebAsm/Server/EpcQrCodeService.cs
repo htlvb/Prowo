44125938c7e66d9db8ab480ace38b7533ea6122c
Johannes Egger <j.egger@posteo.at>
2026-04-23 07:16:06 +0200

    Use smaller logo

--- Prowo.WebAsm/Server/EpcQrCodeService.cs
+++ Prowo.WebAsm/Server/EpcQrCodeService.cs
@@ -1,5 +1,6 @@
 using QRCoder;
 using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.PixelFormats;
 using SixLabors.ImageSharp.Processing;
 
 namespace Prowo.WebAsm.Server;
@@ -16,15 +17,18 @@ public class EpcQrCodeService(byte[] logoBytes)
         var qrPngBytes = pngQrCode.GetGraphic(pixelsPerModule: 10);
 
         using var qrImage = Image.Load(qrPngBytes);
+        using var canvas = new Image<Rgba32>(qrImage.Width, qrImage.Height, Color.White);
+        canvas.Mutate(op => op.DrawImage(qrImage, new Point(0, 0), opacity: 1f));
+
         using var logo = Image.Load(logoBytes);
         var logoSize = qrImage.Width / 5;
         logo.Mutate(op => op.Resize(logoSize, logoSize));
         var x = (qrImage.Width - logoSize) / 2;
         var y = (qrImage.Height - logoSize) / 2;
-        qrImage.Mutate(op => op.DrawImage(logo, new Point(x, y), opacity: 1f));
+        canvas.Mutate(op => op.DrawImage(logo, new Point(x, y), opacity: 1f));
 
         using var ms = new MemoryStream();
-        qrImage.SaveAsPng(ms);
+        canvas.SaveAsPng(ms);
         return Convert.ToBase64String(ms.ToArray());
     }
 
